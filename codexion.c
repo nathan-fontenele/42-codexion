52391d8f02cb28843b3ccb2fd8584f6775fab3aa
Nathan Fontenele <nathan.fontenele@gondimadv.com.br>
2026-05-26 16:38:38 -0300

    feat(main): validacoes de parametros

--- codexion.c
+++ codexion.c
@@ -1,4 +1,32 @@
+#include "codexion.h"
+
 int main (int argc, char **argv)
 {
+    if (argc != 9)
+    {
+        printf("number of invalid arguments.\n");
+        return(1);
+    }
     
+    int i;
+    
+    i = 1;
+    while (i <= 7)
+    {
+        if (atoi(argv[i]) < 0)
+        {
+            printf("Error: Argument %d must be a positive interger.\n", i);
+            return(1);
+        }
+        i++;
+    }
+
+    if (strcmp(argv[8], "fifo") != 0 && strcmp(argv[8], "edf") != 0)
+    {
+        printf("Error: Schedule must be 'fifo' or 'edf'.\n");
+        return(1);
+    }
+
+    printf("Arguments validated. Starting simulation...\n");
+    return(0);
 }
